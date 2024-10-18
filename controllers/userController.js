afa0d9f4e2c1d40595aa4a3e3badefe4ec0122e7
asitshakya678 <asitshakya789@gmail.com>
2024-10-18 22:02:48 +0530

    added controller

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const registerLoad = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+const register = async(req, res)=>{
+    
+}
+
+module.exports ={
+    registerLoad,
+    register
+}
